2a14284919359dcb58a62b10e760e2bbe08df84a
Maelwen THOMAS <thomas.maelwen@gmail.com>
2024-11-13 16:07:30 +0100

    Added read form the device

--- test_json_replica.py
+++ test_json_replica.py
@@ -12,37 +12,61 @@ def print_usb_control_transfer(setup_data, dest):
     wIndex = int(setup_data.get("usb.LanguageId", "0"), 16)
     wLength = int(setup_data["usb.setup.wLength"], 10)
     
-    print(f"Control Transfer: dest={dest}, bmRequestType={bmRequestType}, bRequest={bRequest}, "
+    print(f"Control Transfer (sent to device): dest={dest}, bmRequestType={bmRequestType}, bRequest={bRequest}, "
           f"wValue={wValue}, wIndex={wIndex}, wLength={wLength}")
 
-def print_usb_bulk_transfer(endpoint, data_len, dest):
+def print_usb_bulk_transfer(endpoint, data_len, dest, is_response=False):
     """Print bulk transfer details."""
-    print(f"Bulk Transfer: dest={dest}, endpoint={endpoint}, data_length={data_len}")
+    if is_response:
+        print(f"Bulk Transfer Response (from device): dest={dest}, endpoint={endpoint}, data_length={data_len}")
+    else:
+        print(f"Bulk Transfer (sent to device): dest={dest}, endpoint={endpoint}, data_length={data_len}")
 
-def print_usb_interrupt_transfer(endpoint, data_len, dest):
+def print_usb_interrupt_transfer(endpoint, data_len, dest, is_response=False):
     """Print interrupt transfer details."""
-    print(f"Interrupt Transfer: dest={dest}, endpoint={endpoint}, data_length={data_len}")
+    if is_response:
+        print(f"Interrupt Transfer Response (from device): dest={dest}, endpoint={endpoint}, data_length={data_len}")
+    else:
+        print(f"Interrupt Transfer (sent to device): dest={dest}, endpoint={endpoint}, data_length={data_len}")
 
 # Loop over packets and print based on type
 for packet in packets:
     usb_layer = packet["_source"]["layers"]["usb"]
+    transfer_type = usb_layer["usb.transfer_type"]
+    dest = usb_layer["usb.dst"]  # Get destination address
 
+    # Processing packets sent from host to device
     if usb_layer["usb.src"] == "host":  # Only process packets sent from host
-        transfer_type = usb_layer["usb.transfer_type"]
-        dest = usb_layer["usb.dst"]  # Get destination address
-
         # Handle control transfers (0x00)
         if transfer_type == "0x02" and "Setup Data" in packet["_source"]["layers"]:
             print_usb_control_transfer(packet["_source"]["layers"]["Setup Data"], dest)
 
-        # Handle bulk transfers (0x02)
+        # Handle bulk transfers (0x03)
         elif transfer_type == "0x03":
             endpoint = int(usb_layer["usb.endpoint_address"], 16)
             data_len = int(usb_layer["usb.data_len"])
             print_usb_bulk_transfer(endpoint, data_len, dest)
 
-        # Handle interrupt transfers (0x03)
+        # Handle interrupt transfers (0x01)
         elif transfer_type == "0x01":
             endpoint = int(usb_layer["usb.endpoint_address"], 16)
             data_len = int(usb_layer["usb.data_len"])
             print_usb_interrupt_transfer(endpoint, data_len, dest)
+
+    # Processing packets received from device (responses)
+    else:  # Process packets from device (response)
+        # Handle control transfers (0x00)
+        if transfer_type == "0x02" and "Setup Data" in packet["_source"]["layers"]:
+            print_usb_control_transfer(packet["_source"]["layers"]["Setup Data"], dest)
+
+        # Handle bulk transfers (0x03)
+        elif transfer_type == "0x03":
+            endpoint = int(usb_layer["usb.endpoint_address"], 16)
+            data_len = int(usb_layer["usb.data_len"])
+            print_usb_bulk_transfer(endpoint, data_len, dest, is_response=True)
+
+        # Handle interrupt transfers (0x01)
+        elif transfer_type == "0x01":
+            endpoint = int(usb_layer["usb.endpoint_address"], 16)
+            data_len = int(usb_layer["usb.data_len"])
+            print_usb_interrupt_transfer(endpoint, data_len, dest, is_response=True)
